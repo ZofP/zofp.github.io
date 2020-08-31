5a39fffb7776506f862adbba27324082cc10aecb
PrincePetya <petr.zofak@gmail.com>
2020-08-31 23:54:10 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Route } from "react-router-dom"
+import CssBaseline from '@material-ui/core/CssBaseline'
+
+import { createMuiTheme } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/core/styles';
+
 import './App.css';
+import Navbar from './components/Navbar';
+
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#AF0001",
+    },
+    secondary: {
+      main: "#B0BEC5",
+    },
+  },
+});
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+
+        <Navbar />
+        <div>navbar</div>
+        <div>header</div>
+        <div>portfolio</div>
+      </ThemeProvider>
+    </>
+  )
 }
 
 export default App;
