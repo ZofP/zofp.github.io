ed5e43243c5ccc1e7d3e22b030055c5967944def
PrincePetya <petr.zofak@gmail.com>
2020-09-25 14:46:36 +0200

    changed TimeLine component to use map array method

--- src/components/TimeLine.jsx
+++ src/components/TimeLine.jsx
@@ -12,29 +12,6 @@ import SchoolIcon from "@material-ui/icons/School";
 import { makeStyles } from "@material-ui/core/styles";
 import { useTheme } from "@material-ui/styles";
 
-const timeLineItems = [
-  {
-    from_to: "9/2019 - současnost",
-    position: "GENERAL LEDGER ACCOUNTANT",
-    description:
-      "interní specialista na zlepšování procesů (process improvement)",
-    company: "DXC Technology Czech Republic s.r.o.",
-  },
-  {
-    from_to: "2/2018 – 8/2019",
-    position: "LABOR ACCOUNTING DEPUTY TEAM LEADER",
-    description:
-      "pověřen vedením desetičlenného týmu, který zpracovává data o docházce interních zaměstnanců v Evropě",
-    company: "DXC Technology Czech Republic s.r.o.",
-  },
-  {
-    from_to: "1/2017 – 1/2018",
-    position: "LABOR ACCOUNTING ASSOCIATE PROFESSIONAL",
-    description: "zpracovávání dat o docházce interních zaměstnanců v Německu",
-    company: "DXC Technology Czech Republic s.r.o.",
-  },
-];
-
 const useStyles = makeStyles((theme) => ({
   timeline: {
     marginRight: "1rem !important",
@@ -42,129 +19,116 @@ const useStyles = makeStyles((theme) => ({
       background: theme.palette.primary.main,
     },
     [theme.breakpoints.between("xs", "sm")]: {
-      // "&.vertical-timeline": {
       padding: "0",
       margin: "2em 0",
     },
-    // },
   },
 
   dates: {
     color: "#000",
-    fontWeight: "600 !important",
-    fontSize: "1.2rem !important",
+    fontWeight: "700 !important",
+    fontSize: "1.3rem !important",
   },
 }));
 
 const TimeLine = () => {
   const classes = useStyles();
   const theme = useTheme();
+  const timeLineItems = [
+    {
+      from_to: "9/2019 - současnost",
+      position: "GENERAL LEDGER ACCOUNTANT",
+      description:
+        "interní specialista na zlepšování procesů (process improvement)",
+      company: "DXC Technology Czech Republic s.r.o.",
+      contentStyle: {
+        background: theme.palette.primary.light,
+        color: theme.palette.secondary.light,
+      },
+      contentArrowStyle: {
+        borderRight: `7px solid  ${theme.palette.primary.light}`,
+      },
+      iconStyle: {
+        background: theme.palette.primary.light,
+        color: "#fff",
+      },
+      icon: <WorkIcon />,
+    },
+    {
+      from_to: "2/2018 – 8/2019",
+      position: "LABOR ACCOUNTING DEPUTY TEAM LEADER",
+      description:
+        "pověřen vedením desetičlenného týmu, který zpracovává data o docházce interních zaměstnanců v Evropě",
+      company: "DXC Technology Czech Republic s.r.o.",
+      contentStyle: {
+        background: theme.palette.secondary.light,
+        color: theme.palette.primary.light,
+      },
+      contentArrowStyle: {
+        borderRight: `7px solid  ${theme.palette.secondary.light}`,
+      },
+      iconStyle: {
+        background: theme.palette.primary.light,
+        color: "#fff",
+      },
+      icon: <WorkIcon />,
+    },
+    {
+      from_to: "1/2017 – 1/2018",
+      position: "LABOR ACCOUNTING ASSOCIATE PROFESSIONAL",
+      description:
+        "zpracovávání dat o docházce interních zaměstnanců v Německu",
+      company:
+        "CSC Computer Sciences s.r.o | DXC Technology Czech Republic s.r.o.",
+      contentStyle: {
+        background: theme.palette.secondary.light,
+        color: theme.palette.primary.light,
+      },
+      contentArrowStyle: {
+        borderRight: `7px solid  ${theme.palette.secondary.light}`,
+      },
+      iconStyle: {
+        background: theme.palette.primary.light,
+        color: "#fff",
+      },
+      icon: <WorkIcon />,
+    },
+    {
+      from_to: "2016",
+      position: "Bakalářský titul v oboru Ekonomie a finance",
+      description: "",
+      company: "Univerzita Karlova v Praze",
+      contentStyle: {
+        background: theme.palette.secondary.main,
+        color: theme.palette.primary.dark,
+      },
+      contentArrowStyle: {
+        borderRight: `7px solid  ${theme.palette.secondary.main}`,
+      },
+      iconStyle: {
+        background: theme.palette.secondary.main,
+        color: theme.palette.primary.dark,
+      },
+      icon: <SchoolIcon />,
+    },
+  ];
   return (
     <VerticalTimeline className={classes.timeline}>
-      <VerticalTimelineElement
-        className="vertical-timeline-element--work"
-        contentStyle={{
-          background: theme.palette.primary.light,
-          color: theme.palette.secondary.light,
-        }}
-        contentArrowStyle={{
-          borderRight: `7px solid  ${theme.palette.primary.light}`,
-        }}
-        date={timeLineItems[0].from_to}
-        dateClassName={classes.dates}
-        iconStyle={{
-          background: theme.palette.primary.light,
-          color: "#fff",
-        }}
-        icon={<WorkIcon />}
-      >
-        <h3 className="vertical-timeline-element-title">
-          GENERAL LEDGER ACCOUNTANT
-        </h3>
-        <h4 className="vertical-timeline-element-subtitle">
-          DXC Technology Czech Republic s.r.o.
-        </h4>
-        <p>interní specialista na zlepšování procesů (process improvement)</p>
-      </VerticalTimelineElement>
-      <VerticalTimelineElement
-        className="vertical-timeline-element--work"
-        contentStyle={{
-          background: theme.palette.secondary.light,
-          color: theme.palette.primary.light,
-        }}
-        contentArrowStyle={{
-          borderRight: `7px solid  ${theme.palette.secondary.light}`,
-        }}
-        date="2/2018 – 8/2019"
-        dateClassName={classes.dates}
-        iconStyle={{
-          background: theme.palette.primary.light,
-          color: "#fff",
-        }}
-        icon={<WorkIcon />}
-      >
-        <h3 className="vertical-timeline-element-title">
-          LABOR ACCOUNTING DEPUTY TEAM LEADER
-        </h3>
-        <h4 className="vertical-timeline-element-subtitle">
-          DXC Technology Czech Republic s.r.o.
-        </h4>
-        <p>
-          pověřen vedením desetičlenného týmu, který zpracovává data o docházce
-          interních zaměstnanců v Evropě
-        </p>
-      </VerticalTimelineElement>
-      <VerticalTimelineElement
-        className="vertical-timeline-element--work"
-        contentStyle={{
-          background: theme.palette.secondary.light,
-          color: theme.palette.primary.light,
-        }}
-        contentArrowStyle={{
-          borderRight: `7px solid  ${theme.palette.secondary.light}`,
-        }}
-        date="1/2017 – 1/2018"
-        dateClassName={classes.dates}
-        iconStyle={{
-          background: theme.palette.primary.light,
-          color: "#fff",
-        }}
-        icon={<WorkIcon />}
-      >
-        <h3 className="vertical-timeline-element-title">
-          LABOR ACCOUNTING ASSOCIATE PROFESSIONAL
-        </h3>
-        <h4 className="vertical-timeline-element-subtitle">
-          CSC Computer Sciences s.r.o.
-        </h4>
-        <p>zpracovávání dat o docházce interních zaměstnanců v Německu</p>
-      </VerticalTimelineElement>
-
-      <VerticalTimelineElement
-        className="vertical-timeline-element--education"
-        contentStyle={{
-          background: theme.palette.secondary.main,
-          color: theme.palette.primary.dark,
-        }}
-        contentArrowStyle={{
-          borderRight: `7px solid  ${theme.palette.secondary.main}`,
-        }}
-        date="2016"
-        dateClassName={classes.dates}
-        iconStyle={{
-          background: theme.palette.secondary.main,
-          color: theme.palette.primary.dark,
-        }}
-        icon={<SchoolIcon />}
-      >
-        <h3 className="vertical-timeline-element-title">
-          Bakalářský titul v oboru Ekonomie a finance
-        </h3>
-        <h4 className="vertical-timeline-element-subtitle">
-          Univerzita Karlova v Praze
-        </h4>
-        <p></p>
-      </VerticalTimelineElement>
+      {timeLineItems.map((item) => (
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          contentStyle={item.contentStyle}
+          contentArrowStyle={item.contentArrowStyle}
+          date={item.from_to}
+          dateClassName={classes.dates}
+          iconStyle={item.iconStyle}
+          icon={item.icon}
+        >
+          <h3 className="vertical-timeline-element-title">{item.position}</h3>
+          <h4 className="vertical-timeline-element-subtitle">{item.company}</h4>
+          <p>{item.description}</p>
+        </VerticalTimelineElement>
+      ))}
     </VerticalTimeline>
   );
 };
