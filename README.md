a395a4e4d88ab98a4352f09cdd410651581d6ddc
ZofP <petr.zofak@gmail.com>
2021-01-02 18:17:52 +0100

    Updates

--- README.md
+++
@@ -1,3 +0,0 @@
-## Personal Portfolio (Petr Žofák)
-
-My personal portfolio web page. Made from scratch.
---
+++ precache-manifest.eef1016aa33aa81fa982910401ebe4cc.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de08a74b14d0c4a1af58fa5ccb211a21",
+    "url": "/index.html"
+  },
+  {
+    "revision": "871e37dacadc2b415f3a",
+    "url": "/static/css/2.22ca87da.chunk.css"
+  },
+  {
+    "revision": "eb902b9f10b9607c3d4d",
+    "url": "/static/css/main.44f7c7e7.chunk.css"
+  },
+  {
+    "revision": "871e37dacadc2b415f3a",
+    "url": "/static/js/2.155f740c.chunk.js"
+  },
+  {
+    "revision": "f02286162cae4010569fdc9a00ea720f",
+    "url": "/static/js/2.155f740c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb902b9f10b9607c3d4d",
+    "url": "/static/js/main.93794008.chunk.js"
+  },
+  {
+    "revision": "1e8a0c66ac637b073989",
+    "url": "/static/js/runtime-main.a92b161d.js"
+  },
+  {
+    "revision": "aeaec6b00175745a8000989ccdfb808b",
+    "url": "/static/media/avatar.aeaec6b0.png"
+  },
+  {
+    "revision": "1bc167c56564e36f63c72b0683bd3ea4",
+    "url": "/static/media/backroads.1bc167c5.jpeg"
+  },
+  {
+    "revision": "e2e17bdf45c0c3ec898bdf5e891095c7",
+    "url": "/static/media/coffeebar.e2e17bdf.jpeg"
+  },
+  {
+    "revision": "fcf0fa9b6201d91c89cb4033776ec761",
+    "url": "/static/media/pixabay.fcf0fa9b.jpg"
+  }
+]);
