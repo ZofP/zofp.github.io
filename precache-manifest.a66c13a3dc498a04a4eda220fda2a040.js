33c86581ad92c3badaadbc510f7d375c6fb1a830
ZofP <petr.zofak@gmail.com>
2021-11-29 13:36:44 +0100

    Updates

---
+++ precache-manifest.a66c13a3dc498a04a4eda220fda2a040.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d55f204ed06f98a68142d1f34e261a45",
+    "url": "/index.html"
+  },
+  {
+    "revision": "42dfd50d6e2552ce1d76",
+    "url": "/static/css/2.22ca87da.chunk.css"
+  },
+  {
+    "revision": "b5e46c04cbc871bea0af",
+    "url": "/static/css/main.44f7c7e7.chunk.css"
+  },
+  {
+    "revision": "42dfd50d6e2552ce1d76",
+    "url": "/static/js/2.e7a7899d.chunk.js"
+  },
+  {
+    "revision": "66c1de1f3a9177e55136ceb56bb03fdb",
+    "url": "/static/js/2.e7a7899d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5e46c04cbc871bea0af",
+    "url": "/static/js/main.f0143136.chunk.js"
+  },
+  {
+    "revision": "1e8a0c66ac637b073989",
+    "url": "/static/js/runtime-main.a92b161d.js"
+  },
+  {
+    "revision": "aeaec6b00175745a8000989ccdfb808b",
+    "url": "/static/media/avatar.aeaec6b0.png"
+  },
+  {
+    "revision": "fcf0fa9b6201d91c89cb4033776ec761",
+    "url": "/static/media/pixabay.fcf0fa9b.jpg"
+  },
+  {
+    "revision": "0dc3f3813542d59cecf520e5906adeac",
+    "url": "/static/media/youtube-clone.0dc3f381.png"
+  }
+]);
